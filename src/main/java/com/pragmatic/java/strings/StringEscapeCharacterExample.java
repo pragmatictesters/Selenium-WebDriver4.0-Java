65332fc124085a0d032ffe5e8cb0987b864f77fb
Janesh Kodikara <janesh@pragmatictesters.com>
2022-07-11 20:36:22 +0530

    Add string escape sequence example

---
+++ src/main/java/com/pragmatic/java/strings/StringEscapeCharacterExample.java
@@ -0,0 +1,12 @@
+package com.pragmatic.java.strings;
+
+public class StringEscapeCharacterExample {
+
+
+    public static void main(String[] args) {
+        System.out.println("This contains double quote \" Hello Java \"");
+        System.out.println("This contains new line  \rI am in a new line  ");
+        System.out.println("This contains Tab\t\tI am two tabs away");
+        System.out.println("");
+    }
+}

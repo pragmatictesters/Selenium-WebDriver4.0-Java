7db71908c595f4652f5f9dd67eae6624eeab0bd6
Janesh Kodikara <janesh@pragmatictesters.com>
2022-08-04 00:21:12 +0530

    Apply changes suggested by IDEA in EmployeeMaster.java

--- src/main/java/com/pragmatic/util/EmployeeMaster.java
+++ src/main/java/com/pragmatic/util/EmployeeMaster.java
@@ -2,36 +2,33 @@ package com.pragmatic.util;
 
 import com.github.javafaker.Faker;
 
-import java.util.Random;
-
 public class EmployeeMaster {
 
 
     public static Employee getEmployee() {
 
         String firstName = getFirstName();
-        String lastName = getFirstName();
+        String lastName = getLastName();
         var empNumber = getEmployeeNumber();
-        Employee employee = new Employee(firstName, lastName, empNumber);
-        return employee;
+        return new Employee(firstName, lastName, empNumber);
     }
 
-    private static String getEmployeeNumber() {
+    public static String getEmployeeNumber() {
         return getEmployeeNumber("RC-QA");
     }
 
-    private static String getEmployeeNumber(String prefix) {
+    public static String getEmployeeNumber(String prefix) {
         Faker faker = new Faker();
-        var suffix = faker.number().digits(5).toString();
+        var suffix = faker.number().digits(5);
         return String.format("%s-%s", prefix, suffix) ;
     }
 
-    private static String getFirstName() {
+    public static String getFirstName() {
         var faker = new Faker();
         return faker.name().firstName().replaceAll("'", "");
     }
 
-    private static String getLastName() {
+    public static String getLastName() {
         var faker = new Faker();
         return faker.name().lastName().replaceAll("'", "");
     }

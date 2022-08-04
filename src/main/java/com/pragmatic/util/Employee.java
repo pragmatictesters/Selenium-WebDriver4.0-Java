a3b7859a97815a7b1da3d6769e143a5c7d7caf29
Janesh Kodikara <janesh@pragmatictesters.com>
2022-08-04 10:02:11 +0530

    Remove day2 solutions from day3 branch

--- src/main/java/com/pragmatic/util/Employee.java
+++
@@ -1,8 +0,0 @@
-package com.pragmatic.util;
-
-public record Employee(String firstName, String lastName, String empNumber) {
-
-    public String getFullName(){
-        return String.format("%s %s", firstName(), lastName());
-    }
-}

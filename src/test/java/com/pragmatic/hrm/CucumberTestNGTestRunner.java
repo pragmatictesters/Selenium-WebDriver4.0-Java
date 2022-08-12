8f518d5dd1e41b6fae0003f27400773965d0ca63
Janesh Kodikara <janesh@pragmatictesters.com>
2022-08-12 10:47:43 +0530

    Add cucumber.properties and CucumberOptions

--- src/test/java/com/pragmatic/hrm/CucumberTestNGTestRunner.java
+++ src/test/java/com/pragmatic/hrm/CucumberTestNGTestRunner.java
@@ -5,6 +5,10 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(tags = "",
         features = {"src/test/resources/features/Login.feature"},
-        glue = {"com.pragmatic.hrm.steps"})
+        glue = {"com.pragmatic.hrm.steps"},
+        dryRun = true,
+        monochrome = false,
+        publish = true
+)
 public class CucumberTestNGTestRunner extends AbstractTestNGCucumberTests {
 }

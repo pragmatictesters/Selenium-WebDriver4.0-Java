3d158bd465d33edf2fa948bb65b9a4c55a7a3a22
Janesh Kodikara <janesh@pragmatictesters.com>
2022-08-14 14:24:01 +0530

    Add @Test timeout example

--- src/test/java/com/pragmatic/testng/group1/TestNGExample2.java
+++ src/test/java/com/pragmatic/testng/group1/TestNGExample2.java
@@ -1,21 +1,25 @@
 package com.pragmatic.testng.group1;
 
 import org.testng.annotations.Test;
+import org.testng.internal.thread.ThreadTimeoutException;
 
 public class TestNGExample2 {
 
 
-    @Test
-    public void testMethod1() {
+    @Test(timeOut = 2000)
+    public void testMethod1() throws InterruptedException {
         System.out.println("TestNGExample2.testMethod1");
+        Thread.sleep(1200);
     }
-     @Test
-    public void testMethod2() {
+     @Test(timeOut = 2000, expectedExceptions = {ThreadTimeoutException.class})
+    public void testMethod2() throws InterruptedException {
         System.out.println("TestNGExample2.testMethod2");
+        Thread.sleep(2100);
     }
-     @Test
-    public void testMethod3() {
+     @Test(timeOut = 2000)
+    public void testMethod3() throws InterruptedException {
         System.out.println("TestNGExample2.testMethod3");
+         Thread.sleep(2100);
     }
      @Test
     public void testMethod4() {
